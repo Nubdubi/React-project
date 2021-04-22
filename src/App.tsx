db5c3df14d7f0e7de602cb3c0721f6a32dbcb3ed
Nubdubi <47843014+Nubdubi@users.noreply.github.com>
2021-04-22 15:09:59 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Main from './Page/Main/Main';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Main/>
+
+    </div>
+  );
+}
+
+export default App;
